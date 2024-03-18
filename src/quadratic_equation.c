5a71ce73dc391b5259973eceb408bb750ea3d8f5
romanov-ivan-al <iromanov06051992@gmail.com>
2024-03-18 16:20:07 +0700

    add files and folder

---
+++ src/quadratic_equation.c
@@ -0,0 +1,9 @@
+#include "quadratic_equation.h"
+
+double solve_equation(double a, double b, double c) {
+
+}
+
+double discriminant(double a, double b, double c) {
+    
+}
---
+++ src/quadratic_equation.h
@@ -0,0 +1,9 @@
+#ifndef SRC_QUADRATIC_EQUATION_H
+#define SRC_QUADRATIC_EQUATION_H
+
+#include <stdio.h>
+
+double solve_equation(double a, double b, double c);
+double discriminant(double a, double b, double c);
+
+#endif
