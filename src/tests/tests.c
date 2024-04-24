5abfac677182b2af179437c6bfe40e83e493abee
romanov-ivan-al <iromanov06051992@gmail.com>
2024-04-24 16:32:15 +0700

    edited tests

--- src/tests/tests.c
+++ src/tests/tests.c
@@ -24,73 +24,73 @@ START_TEST(test_solve_equation_one_root)
 }
 END_TEST
 
-// START_TEST(test_solve_equation_two_roots)
-// {
-//     double a = 1.0;
-//     double b = 5.0;
-//     double c = 6.0;
-//     int count_roots = solve_equation(a, b, c);
-//     ck_assert_int_eq(2, count_roots);
-// }
-// END_TEST
-// START_TEST(test_discriminant_positive)
-// {
-//     double a = 1.0;
-//     double b = -3.0;
-//     double c = 2.0;
-//     double result = discriminant(a, b, c);
-//     ck_assert_double_eq(result, 1.0);
-// }
-// END_TEST
+START_TEST(test_solve_equation_two_roots)
+{
+    double a = 1.0;
+    double b = 5.0;
+    double c = 6.0;
+    int count_roots = solve_equation(a, b, c);
+    ck_assert_int_eq(2, count_roots);
+}
+END_TEST
+START_TEST(test_discriminant_positive)
+{
+    double a = 1.0;
+    double b = -3.0;
+    double c = 2.0;
+    double result = discriminant(a, b, c);
+    ck_assert_double_eq(result, 1.0);
+}
+END_TEST
 
-// START_TEST(test_discriminant_zero)
-// {
-//     double a = 1.0;
-//     double b = -2.0;
-//     double c = 1.0;
-//     double result = discriminant(a, b, c);
-//     ck_assert_double_eq(result, 0.0);
-// }
-// END_TEST
+START_TEST(test_discriminant_zero)
+{
+    double a = 1.0;
+    double b = -2.0;
+    double c = 1.0;
+    double result = discriminant(a, b, c);
+    ck_assert_double_eq(result, 0.0);
+}
+END_TEST
 
-// START_TEST(test_discriminant_negative)
-// {
-//     double a = 2.0;
-//     double b = 1.0;
-//     double c = 3.0;
-//     double result = discriminant(a, b, c);
-//     ck_assert_double_eq(result, -23.0);
-// }
-// END_TEST
+START_TEST(test_discriminant_negative)
+{
+    double a = 2.0;
+    double b = 1.0;
+    double c = 3.0;
+    double result = discriminant(a, b, c);
+    ck_assert_double_eq(result, -23.0);
+}
+END_TEST
 
-// START_TEST(test_count_root)
-// {
-//     double a = 1.0;
-//     double b = -3.0;
-//     double disc = 1.0;
-//     double result = calculate_quadratic_roots(a, b, disc);
-//     ck_assert_double_eq(result, 2.0);
-// }
-// END_TEST
+START_TEST(test_count_root)
+{
+    double a = 1.0;
+    double b = -3.0;
+    double disc = 1.0;
+    double result = calculate_quadratic_roots(a, b, disc);
+    ck_assert_double_eq(result, 2.0);
+}
+END_TEST
 
 int main(void)
 {
     Suite *s1 = suite_create("Quadratic Equation");
     TCase *tc1 = tcase_create("Solve Equation");
-    // TCase *tc2 = tcase_create("Discriminant and Count Root");
+    TCase *tc2 = tcase_create("Discriminant and Count Root");
     SRunner *sr = srunner_create(s1);
     int nf;
 
     suite_add_tcase(s1, tc1);
     tcase_add_test(tc1, test_solve_equation_no_roots);
     tcase_add_test(tc1, test_solve_equation_one_root);
-    // tcase_add_test(tc1, test_solve_equation_two_roots);
+    tcase_add_test(tc1, test_solve_equation_two_roots);
 
-    // suite_add_tcase(s1, tc2);
-    // tcase_add_test(tc2, test_discriminant_positive);
-    // tcase_add_test(tc2, test_discriminant_zero);
-    // tcase_add_test(tc2, test_discriminant_negative);
-    // tcase_add_test(tc2, test_count_root);
+    suite_add_tcase(s1, tc2);
+    tcase_add_test(tc2, test_discriminant_positive);
+    tcase_add_test(tc2, test_discriminant_zero);
+    tcase_add_test(tc2, test_discriminant_negative);
+    tcase_add_test(tc2, test_count_root);
 
     srunner_run_all(sr, CK_ENV);
     nf = srunner_ntests_failed(sr);
